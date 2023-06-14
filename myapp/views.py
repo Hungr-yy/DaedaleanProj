2a1432e86babd55cb0c2599a9492facd1a4d8adf
alphonsuslow <77975955+alphonsuslow@users.noreply.github.com>
2023-06-14 18:02:06 +0800

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -2,18 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .grading import *
 import re
-
-# Create your views here.
-
-'''
-def index(request):
-    # name = 'Alphonsus' # Can be extracted from a database e.g. user.name
-    context = { # context as a dictionary
-        'name': 'Alphonsus',
-        'location': 'Bishan'
-    }
-    return render(request, 'index.html', context)
-'''
+import html
 
 def index(request):
     return render(request, 'index.html')
@@ -21,20 +10,15 @@ def index(request):
 def login(request):
     return render(request, 'login.html')
 
-'''
-def result(request):
-    text = request.POST['text']
-
-    word_count, fre, fkra, fog, cli, smog, ari = grading(text)
-
-    return render(request, 'result.html', {'text': text, 'word_count': word_count, 'fre': fre, 'fkra': fkra, 'fog': fog, 'cli': cli, 'smog': smog, 'ari': ari})
-'''
-
 def result(request):
     text = None
 
     if request.method == 'POST':
         text = request.POST.get('text')
+
+    pattern = r"<.*?>"
+    text = html.unescape(text)
+    text = re.sub(pattern, "", text)
 
     if len(text.split()) == 0:
         exception_string = "No words detected!"
@@ -42,6 +26,9 @@ def result(request):
     elif ((text.count('.') + text.count('!') + text.count('?')) == 0 and len(text.split()) != 0):
         exception_string = "No sentences detected!"
         return render(request, 'index.html', {'exception_string': exception_string})
+    elif text[0] == "!" or text[0] == "?" or text[0] == "." or text[0] == ",":
+        exception_string = "Invalid starting character!"
+        return render(request, 'index.html', {'exception_string': exception_string})
     else:
-        word_count, fre, fkra, cli, smog, ari = grading(text)
-        return render(request, 'index.html', {'text': text, 'word_count': word_count, 'fre': fre, 'fkra': fkra, 'cli': cli, 'smog': smog, 'ari': ari})
+        word_count, sentence_count, fre, fkra, cli, smog, ari = grading(text)
+        return render(request, 'index.html', {'text': text, 'word_count': word_count, 'sentence_count': sentence_count, 'fre': fre, 'fkra': fkra, 'cli': cli, 'smog': smog, 'ari': ari})
