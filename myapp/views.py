dcc31d0ddee39287cdf03151b12f1da501ddc8dd
alphonsuslow <77975955+alphonsuslow@users.noreply.github.com>
2023-06-16 23:59:41 +0800

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -32,23 +32,28 @@ def index(request):
 
 def register(request):
     if request.method == 'POST':
-        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
+
+        if first_name == "" or last_name == "" or email == "" or password == "" or password2 == "":
+            messages.info(request, 'Incomplete fields.')
+            return redirect('register')
 
         if password == password2:
             if User.objects.filter(email=email).exists():
                 messages.info(request, 'This email is already in use.')
                 return redirect('register')
-            elif User.objects.filter(username=username).exists():
-                messages.info(request, 'This username is already in use.')
+            elif User.objects.filter(first_name=first_name, last_name=last_name).exists():
+                messages.info(request, 'This name is already in use.')
                 return redirect('register')
             elif len(password) < 8:
-                messages.info(request, 'Insufficient password length.')
+                messages.info(request, 'Password must be at least 8 characters long.')
                 return redirect('register')
             else:
-                user = User.object.create_user(username=username, email=email, password=password)
+                user = User.object.create_user(first_name=first_name, last_name=last_name, email=email, password=password)
                 user.save()
                 return redirect('login')
         else:
@@ -59,16 +64,16 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST['username']
+        email = request.POST['email']
         password = request.POST['password']
 
-        user = auth.authenticate(username=username, password=password)
+        user = auth.authenticate(email=email, password=password)
 
         if user is not None:
             auth.login(request, user)
             return redirect('/')
         else:
-            messages.info(request, 'Credentials Invalid')
+            messages.info(request, 'Incorrect email or password.')
             return redirect ('login')
     else:
         return render(request, 'login.html')
